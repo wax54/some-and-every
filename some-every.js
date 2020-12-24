4f163b3996532750a237bf3c953eb799aa5d585b
sam <wax654@gmail.com>
2020-12-24 15:33:33 -0800

    Wrote has no duplicates.

--- some-every.js
+++ some-every.js
@@ -53,14 +53,22 @@ function hasOnlyOddNumbers(arr) {
 }
 
 /*
-Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
+Write a function called hasNoDuplicates 
+which accepts an array and returns true 
+if there are no duplicate values 
+(more than one element in the array that has the same value as another). 
+If there are any duplicates, the function should return false.
 
 Examples:
     hasNoDuplicates([1,2,3,1]) // false
     hasNoDuplicates([1,2,3]) // true
 */
 
-function hasNoDuplicates(arr) {}
+function hasNoDuplicates(arr) {
+    return arr.every(function(val, i, array){
+        return i === array.lastIndexOf(val);
+    });
+}
 
 /*
 Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
