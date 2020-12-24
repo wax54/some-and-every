3cf0fbc93fc5fefc03daeaf3e8e5100587aaa233
sam <wax654@gmail.com>
2020-12-24 15:19:43 -0800

    wrote hasAZero

--- some-every.js
+++ some-every.js
@@ -14,14 +14,25 @@ function hasOddNumber(arr) {
     });
 }
 /*
-Write a function called hasAZero which accepts a number and returns true if that number contains at least one zero. Otherwise, the function should return false
+Write a function called hasAZero 
+    which accepts a number and returns true 
+    if that number contains at least one zero. 
+    Otherwise, the function should return false
 
 Examples:
     hasAZero(3332123213101232321) // true
     hasAZero(1212121) // false
 */
 
-function hasAZero(num) {}
+function hasAZero(num) {
+    str = num +'';
+    for(l of str){
+        if(l == '0'){
+            return true;
+        }
+    }
+    return false;
+}
 
 /*
 Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is odd. If any of the values in the array are not odd, the function should return false. 
